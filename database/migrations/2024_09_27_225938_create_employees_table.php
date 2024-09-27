52714cdb903b4931ae0161c863a2e192937d7875
felipe.silva <felipe.silva@pwi.com.br>
2024-09-27 20:17:50 -0300

    feat: Criando migrations

---
+++ database/migrations/2024_09_27_225938_create_employees_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 70);
+            $table->string('cpf', 11);
+            $table->string('registration_number', 100);
+            $table->smallInteger('gender');
+            $table->string('cpts_number', 100);
+            $table->string('cpts_serial', 60);
+            $table->string('cpts_uf', 2);
+            $table->date('cpts_date');
+            $table->date('admission_date');
+            $table->date('dismissal_date');
+            $table->integer('cost_center');
+            $table->string('capacity_unit', 12);
+            $table->decimal('salary', 10, 2);
+            $table->string('rg', 9);
+            $table->string('position_id', 20);
+            $table->string('enterprise_id', 4);
+            $table->timestamps();
+
+            $table->foreign('position_id')->references('id')->on('positions')->onDelete('cascade');
+            $table->foreign('enterprise_id')->references('id')->on('enterprises')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};

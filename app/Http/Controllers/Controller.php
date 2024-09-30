d8ab3adc4a7778379fe48e862196418769ad981d
felipe.silva <felipe.silva@pwi.com.br>
2024-09-30 13:49:49 -0300

    feat: Criando controller base

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,93 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use App\Exceptions\MasterNotFoundHttpException;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+abstract class Controller extends BaseController
 {
-    //
+    protected $model;
+    protected $request;
+    protected $dto;
+
+    public function index() {
+        $query = $this->model::query();
+        $filters = $this->request->all();
+
+        // Filtros extras além do fillable
+        $othersFillableFields = [];
+
+        // Filtra todos os campos que estão na propriedade fillable da model
+        foreach ($filters as $field => $value) {
+            if (in_array($field, $this->model->getFillable()) || in_array($field, $othersFillableFields)) {
+                if (method_exists($this->model, 'rules')){
+                    if ($this->model::rules()[$field] && str_contains($this->model::rules()[$field], 'string')) {
+                        $query->where($field, 'like', '%'.trim($value).'%');
+                        continue;
+                    }
+                }
+
+                $query->where($field, $value);
+            }
+        }
+
+        $query->orderBy('id', 'desc');
+
+        $data = $query->get();
+
+        return response()->json($data, 200);
+    }
+
+    public function show($id) {
+        $data = $this->model::find($id);
+
+        if (!$data) {
+            throw new MasterNotFoundHttpException();
+        }
+
+        if($this->dto && method_exists($this->dto, 'fromModel')) {
+            $data = $this->dto::fromModel($data);
+        }
+        
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request) {
+        if (method_exists($this->model, 'rules')){
+            $request->validate($this->model::rules());
+        }
+        
+        $data = $this->model::create($request->all());
+
+        return response()->json($data, 201);
+    }
+
+    public function update(Request $request, $id) {
+        $data = $this->model::find($id);
+
+        if (!$data) {
+            throw new MasterNotFoundHttpException;
+        }
+
+        if (method_exists($this->model, 'rules')){
+            $request->validate($this->model::rules());
+        }
+            
+        $data->update($request->all());
+
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id) {
+        $data = $this->model::find($id);
+
+        if (!$data) {
+            throw new MasterNotFoundHttpException;
+        }
+
+        $data->delete();
+
+        return response()->json(['message' => 'Registro deletado com sucesso!'], 200);
+    }
 }

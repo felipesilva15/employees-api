bc38001053007bf2b9a26cd1c7d27e583eec121a
Felipe Silva <felipe.allware@gmail.com>
2024-10-05 20:03:56 -0300

    docs: Criando documentação no Swagger de Enterprise

--- app/Http/Controllers/EnterpriseController.php
+++ app/Http/Controllers/EnterpriseController.php
@@ -11,4 +11,173 @@ class EnterpriseController extends Controller
         $this->model = $model;
         $this->request = $request;
     }
+
+    /**
+     * @OA\Get(
+     *      path="/api/enterprise",
+     *      tags={"Enterprise"},
+     *      summary="List all enterprises",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         description="Enterprise ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="name",
+     *         in="query",
+     *         description="Enterprise name",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Enterprise list",
+     *          @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Enterprise")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401",
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function index() {
+        return parent::index();
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/api/enterprise/{id}",
+     *      tags={"Enterprise"},
+     *      summary="List an enterprise by ID",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Enterprise ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Enterprise data",
+     *          @OA\JsonContent(ref="#/components/schemas/Enterprise")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function show($id) {
+        return parent::show($id);
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/api/enterprise",
+     *      tags={"Enterprise"},
+     *      summary="Registers an enterprise",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for creating a new enterprise",
+     *         @OA\JsonContent(ref="#/components/schemas/Enterprise")
+     *      ),
+     *      @OA\Response(
+     *          response="201", 
+     *          description="Registered enterprise data",
+     *          @OA\JsonContent(ref="#/components/schemas/Enterprise")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function store(Request $request) {
+        return parent::store($request);
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/api/enterprise/{id}",
+     *      tags={"Enterprise"},
+     *      summary="Update an enterprise",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Enterprise ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for update enterprise",
+     *         @OA\JsonContent(ref="#/components/schemas/Enterprise")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Updated enterprise data",
+     *          @OA\JsonContent(ref="#/components/schemas/Enterprise")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function update(Request $request, $id) {
+        return parent::update($request, $id);
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/api/enterprise/{id}",
+     *      tags={"Enterprise"},
+     *      summary="Deletes an enterprise",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Enterprise ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Return message",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string", example="Registro deletado com sucesso!")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function destroy( $id) {
+        return parent::destroy($id);
+    }
 }
--- app/Models/Enterprise.php
+++ app/Models/Enterprise.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *      schema="Enterprise",
+ *      required={"id", "name"},
+ *      @OA\Property(property="id", type="string", example="1", maxLength=4),
+ *      @OA\Property(property="name", type="string", example="Base Sede", maxLength=40),
+ *      @OA\Property(property="created_at", type="string", format="date-time", example="2024-01-30T03:00:00.000000Z"),
+ *      @OA\Property(property="updated_at", type="string", format="date-time", example="2024-01-30T03:00:00.000000Z")
+ * )
+ */
 class Enterprise extends Model
 {
     use HasFactory;

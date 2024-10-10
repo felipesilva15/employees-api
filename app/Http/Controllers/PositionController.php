a96144d581b9232e537102ff1f828e3381babb9f
Felipe Silva <felipe.allware@gmail.com>
2024-10-10 19:22:55 -0300

    docs: Documentando recurso de Position com Swagger

--- app/Http/Controllers/PositionController.php
+++ app/Http/Controllers/PositionController.php
@@ -11,4 +11,173 @@ class PositionController extends Controller
         $this->model = $model;
         $this->request = $request;
     }
+
+    /**
+     * @OA\Get(
+     *      path="/api/position",
+     *      tags={"Position"},
+     *      summary="List all positions",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         description="Position ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="name",
+     *         in="query",
+     *         description="Position name",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Position list",
+     *          @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Position")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401",
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function index() {
+        return parent::index();
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/api/position/{id}",
+     *      tags={"Position"},
+     *      summary="List an position by ID",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Position ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Position data",
+     *          @OA\JsonContent(ref="#/components/schemas/Position")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function show($id) {
+        return parent::show($id);
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/api/position",
+     *      tags={"Position"},
+     *      summary="Registers an position",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for creating a new position",
+     *         @OA\JsonContent(ref="#/components/schemas/Position")
+     *      ),
+     *      @OA\Response(
+     *          response="201", 
+     *          description="Registered position data",
+     *          @OA\JsonContent(ref="#/components/schemas/Position")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function store(Request $request) {
+        return parent::store($request);
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/api/position/{id}",
+     *      tags={"Position"},
+     *      summary="Update an position",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Position ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for update position",
+     *         @OA\JsonContent(ref="#/components/schemas/Position")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Updated position data",
+     *          @OA\JsonContent(ref="#/components/schemas/Position")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function update(Request $request, $id) {
+        return parent::update($request, $id);
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/api/position/{id}",
+     *      tags={"Position"},
+     *      summary="Deletes an position",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Position ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Return message",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string", example="Registro deletado com sucesso!")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function destroy( $id) {
+        return parent::destroy($id);
+    }
 }
--- app/Models/Position.php
+++ app/Models/Position.php
@@ -5,6 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *      schema="Position",
+ *      required={"id", "name"},
+ *      @OA\Property(property="id", type="string", example="4058", maxLength=20),
+ *      @OA\Property(property="name", type="string", example="Analista de sistemas III", maxLength=80),
+ *      @OA\Property(property="created_at", type="string", format="date-time", example="2024-01-30T03:00:00.000000Z"),
+ *      @OA\Property(property="updated_at", type="string", format="date-time", example="2024-01-30T03:00:00.000000Z")
+ * )
+ */
 class Position extends Model
 {
     use HasFactory;

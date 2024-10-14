c4bda7bc12d84a6cc61740e21d4e4f33b03cd6fc
Felipe Silva <felipe.allware@gmail.com>
2024-10-14 20:24:49 -0300

    docs: Criando documentação de employees no Swagger

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -11,4 +11,263 @@ class EmployeeController extends Controller
         $this->model = $model;
         $this->request = $request;
     }
+
+    /**
+     * @OA\Get(
+     *      path="/api/employee",
+     *      tags={"Employee"},
+     *      summary="List all employees",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         description="Employee ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="name",
+     *         in="query",
+     *         description="Employee name",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="cpf",
+     *         in="query",
+     *         description="Employee CPF",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="registration_number",
+     *         in="query",
+     *         description="Employee registration number",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="gender",
+     *         in="query",
+     *         description="Employee gender",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="cpts_number",
+     *         in="query",
+     *         description="Employee CPTS number",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="cpts_serial",
+     *         in="query",
+     *         description="Employee CPTS serial",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="cpts_uf",
+     *         in="query",
+     *         description="Employee CPTS UF",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="cpts_date",
+     *         in="query",
+     *         description="Employee CPTS date",
+     *         @OA\Schema(type="string", format="date")
+     *      ),
+     *      @OA\Parameter(
+     *         name="admission_date",
+     *         in="query",
+     *         description="Employee admission date",
+     *         @OA\Schema(type="string", format="date")
+     *      ),
+     *      @OA\Parameter(
+     *         name="dismissal_date",
+     *         in="query",
+     *         description="Employee dismissal date",
+     *         @OA\Schema(type="string", format="date")
+     *      ),
+     *      @OA\Parameter(
+     *         name="cost_center",
+     *         in="query",
+     *         description="Employee cost center",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="capacity_unit",
+     *         in="query",
+     *         description="Employee capacity unit",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="salary",
+     *         in="query",
+     *         description="Employee salary",
+     *         @OA\Schema(type="number", format="float")
+     *      ),
+     *      @OA\Parameter(
+     *         name="rg",
+     *         in="query",
+     *         description="Employee RG",
+     *         @OA\Schema(type="string")
+     *      ),
+     *      @OA\Parameter(
+     *         name="position_id",
+     *         in="query",
+     *         description="Employee position ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Parameter(
+     *         name="enterprise_id",
+     *         in="query",
+     *         description="Employee enterprise ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Employee list",
+     *          @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Employee")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401",
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function index() {
+        return parent::index();
+    }
+
+    /**
+     * @OA\Get(
+     *      path="/api/employee/{id}",
+     *      tags={"Employee"},
+     *      summary="List an employee by ID",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Employee ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Employee data",
+     *          @OA\JsonContent(ref="#/components/schemas/Employee")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function show($id) {
+        return parent::show($id);
+    }
+
+    /**
+     * @OA\Post(
+     *      path="/api/employee",
+     *      tags={"Employee"},
+     *      summary="Registers an employee",
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for creating a new employee",
+     *         @OA\JsonContent(ref="#/components/schemas/Employee")
+     *      ),
+     *      @OA\Response(
+     *          response="201", 
+     *          description="Registered employee data",
+     *          @OA\JsonContent(ref="#/components/schemas/Employee")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function store(Request $request) {
+        return parent::store($request);
+    }
+
+    /**
+     * @OA\Put(
+     *      path="/api/employee/{id}",
+     *      tags={"Employee"},
+     *      summary="Update an employee",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Employee ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\RequestBody(
+     *         required=true,
+     *         description="Data for update employee",
+     *         @OA\JsonContent(ref="#/components/schemas/Employee")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Updated employee data",
+     *          @OA\JsonContent(ref="#/components/schemas/Employee")
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function update(Request $request, $id) {
+        return parent::update($request, $id);
+    }
+
+    /**
+     * @OA\Delete(
+     *      path="/api/employee/{id}",
+     *      tags={"Employee"},
+     *      summary="Deletes an employee",
+     *      @OA\Parameter(
+     *         name="id",
+     *         in="path",
+     *         required=true,
+     *         description="Employee ID",
+     *         @OA\Schema(type="integer")
+     *      ),
+     *      @OA\Response(
+     *          response="200", 
+     *          description="Return message",
+     *          @OA\JsonContent(
+     *             type="object",
+     *             @OA\Property(property="message", type="string", example="Registro deletado com sucesso!")
+     *         )
+     *      ),
+     *      @OA\Response(
+     *          response="401", 
+     *          description="Unauthorized",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      ),
+     *      @OA\Response(
+     *          response="404", 
+     *          description="Record not found",
+     *          @OA\JsonContent(ref="#/components/schemas/ApiErrorDTO")
+     *      )
+     * )
+     */
+    public function destroy( $id) {
+        return parent::destroy($id);
+    }
 }
--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,6 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *      schema="Employee",
+ *      required={"name", "cpf", "registration_number", "gender", "admission_date", "cost_center", "capacity_unit", "salary", "rg", "position_id", "enterprise_id"},
+ *      @OA\Property(property="id", type="integer", example=1),
+ *      @OA\Property(property="name", type="string", example="Anderson Nunes", maxLength=70),
+ *      @OA\Property(property="cpf", type="string", example="05645221045", minLength=11, maxLength=11),
+ *      @OA\Property(property="registration_number", type="string", example="35468598", maxLength=100),
+ *      @OA\Property(property="gender", type="integer", example=1, maximum=2),
+ *      @OA\Property(property="cpts_number", type="string", example="818232", maxLength=100),
+ *      @OA\Property(property="cpts_serial", type="string", example="8847", maxLength=60),
+ *      @OA\Property(property="cpts_uf", type="string", example="SP", maxLength=2),
+ *      @OA\Property(property="cpts_date", type="string", format="date", example="2022-01-01"),
+ *      @OA\Property(property="admission_date", type="string", format="date", example="2022-01-01"),
+ *      @OA\Property(property="dismissal_date", type="string", format="date", example=null),
+ *      @OA\Property(property="cost_center", type="integer", example=1),
+ *      @OA\Property(property="capacity_unit", type="string", example="411405040101", maxLength=12),
+ *      @OA\Property(property="salary", type="number", format="float", example=2015.90),
+ *      @OA\Property(property="rg", type="string", example="55698546X", maxLength=9),
+ *      @OA\Property(property="position_id", type="integer", example=1),
+ *      @OA\Property(property="enterprise_id", type="integer", example=1),
+ *      @OA\Property(property="created_at", type="string", format="date-time", example="2024-01-30T03:00:00.000000Z"),
+ *      @OA\Property(property="updated_at", type="string", format="date-time", example="2024-01-30T03:00:00.000000Z")
+ * )
+ */
 class Employee extends Model
 {
     use HasFactory;
